406423e124b95c78b098e1144dd20e0be087ac0d
w0650113 <ngu.hoang@selu.edu>
2016-11-15 14:40:17 -0600

    finish

--- CREATE.sql
+++ CREATE.sql
@@ -1,29 +1,31 @@
+create table MAJOR(
+	Major varchar(32) not null,
+	Req_Course varchar(8) not null,
+	primary key(Major,Req_Course),
+	foreign key(Req_Course) references COURSE(C_Nbr)
+
+);
 create table STUDENT(
 	W_Nbr varchar(8) not null,
 	Name varchar (60) not null,
-	Major varchar (50) not null,
+	Major varchar(32) not null,
 	Gender varchar(10) not null check (Gender in('M', 'F')),
 	Year varchar (20) not null check(Year in('Freshman', 'Sophomore','Junior','Senior')),
 	Gpa float not null,
+	foreign key(Major) references MAJOR(Major),
 	primary key (W_Nbr)
+	
 );
 create table DEPARTMENT(
 	Dept_Name varchar(4) not null,
 	Building varchar(32) not null,
 	primary key(Dept_Name)
 );
-create table INSTRUCTOR(
-	Id int identity not null primary key,
-	Name varchar(64) not null,
-	Dept_Name varchar(4) not null,
-	foreign key(Dept_Name) references DEPARTMENT(Dept_Name)
-);
-create table ADVISOR(
-	W_Nbr varchar(8) not null,
-	I_Id int not null,
-	primary key(W_Nbr,I_Id),
-	foreign key(I_Id) references INSTRUCTOR(Id),
-	foreign key (W_Nbr) references STUDENT(W_Nbr)
+create table PREREQ(
+	C_Nbr varchar(8) not null,
+	P_Nbr varchar(8) not null,
+	primary key(C_Nbr,P_Nbr),
+	foreign key(C_Nbr) references COURSE(C_Nbr)
 );
 create table COURSE(
 	C_Nbr varchar(8) not null,
@@ -32,13 +34,17 @@ create table COURSE(
 	Credits int not null,
 	primary key(C_Nbr),
 	foreign key(Dept_Name) references DEPARTMENT(Dept_Name)
+
 );
-create table PREREQ(
-	C_Nbr varchar(8) not null,
-	P_Nbr varchar(8) not null,
-	primary key(C_Nbr,P_Nbr),
-	foreign key(C_Nbr) references COURSE(C_Nbr)
+
+create table INSTRUCTOR(
+	Id int identity not null primary key,
+	Name varchar(64) not null,
+	Dept_Name varchar(4) not null,
+	foreign key(Dept_Name) references DEPARTMENT(Dept_Name)
 );
+
+
 create table TAKE (
 	W_Nbr varchar(8) not null,
 	Sec_Id int not null,
@@ -46,29 +52,17 @@ create table TAKE (
 	primary key (W_Nbr,Sec_Id),
 	foreign key(W_Nbr) references STUDENT(W_Nbr)
 );
-create table TIME_SLOT(
-	Id int not null,
-	Sec_Nbr int not null,
-	Day varchar(10) check(Day in('Monday','Tuesday','Wednesday','Thursday','Friday')),
-	Start_Time varchar(8) not null,
-	End_Time varchar (8) not null,
-	primary key(Id),
-	unique (Sec_Nbr)
-	
-);
 
-create table SECTION(
+create table SECTION (
 	Id int identity not null,
 	C_Nbr varchar(8) not null,
 	Sec_Nbr int not null,
 	Semester varchar(6) not null check (Semester in('Spring','Summer','Fall')),
 	Year  int not null ,
-	Building varchar(16) not null,
+	Building varchar(32) not null,
 	Room_Nbr int,
-	TimeSlot_Id int not null,
 	Instructor_Id int not null,
 	primary key(Id),
 	foreign key(C_Nbr) references COURSE(C_Nbr),
-	foreign key(TimeSlot_Id) references TIME_SLOT(Id),
 	foreign key(Instructor_Id) references INSTRUCTOR(Id)
 );
--- INSERT.sql
+++ INSERT.sql
@@ -2,14 +2,14 @@ insert into STUDENT
 (W_Nbr,Name,Major,Gender,Year,Gpa)
 values
 ('w0650113','Neil Hoang','Information Technology','M','Senior',3.7),
-('w0112365','John Smith','Computer Science','M','Sophmore',3.5),
-('w0256986','Franklin Wong','Computer Science','M','Junior',2.5),
+('w0112365','John Smith','Information Technology','M','Sophomore',3.5),
+('w0256986','Franklin Wong','Information Technology','M','Junior',2.5),
 ('w0269846','Alicia Zelaya','Information Technology','F','Freshman',3.8),
-('w0245698','Jennifer Wallace','Computer Science','F','Junior',3.9),
-('w0456983','Ramesh Narayan','Computer Science','M','Freshman',3.8),
+('w0245698','Jennifer Wallace','Information Technology','F','Junior',3.9),
+('w0456983','Ramesh Narayan','Information Technology','M','Freshman',3.8),
 ('w0569873','Joyce English','Information Technology','F','Sophomore',3.4),
-('w0569874','Ahmad Jabbar','Computer Science','M','Senior',3.5),
-('w3698745','James Borg','Computer Scinece','M','Freshman',4.0);
+('w0569874','Ahmad Jabbar','Information Technology','M','Senior',3.5),
+('w3698745','James Borg','Information Technology','M','Freshman',4.0);
  
 insert into DEPARTMENT
 (Dept_Name,Building)
@@ -17,7 +17,7 @@ values
 ('CSIT','Fayard Hall');
 
 insert into INSTRUCTOR
-(Id,Name,Dept_Name)
+(Name,Dept_Name)
 values
 ('Ghassan Alkadi','CSIT'),
 ('Kuo-Pao Yang','CSIT'),
@@ -25,38 +25,48 @@ values
 ('Wesley Deneke','CSIT'),
 ('Matthew Gill','CSIT');
 
-insert into ADVISOR
-(W_Nbr,I_Id)
-values
-('w0650113',1),
-('w0112365',2),
-('w0256986',3),
-('w0269846',4),
-('w0245698',5),
-('w0456983',1),
-('w0569873',2),
-('w0569874',3),
-('w3698745',5);
-
 insert into COURSE
 (C_Nbr,Title,Dept_Name,Credits)
 values
-('CMPS 339','Database Administration','CSIT',3),
 ('CMPS 161','Algorithm Design & Implementation I','CSIT',3),
+('CMPS 257','Discrete Structures','CSIT',3),
+('CMPS 280','Algorithm Design & Implementatio II','CSIT',3),
 ('CMPS 285','Software Engineer','CSIT',3),
+('CMPS 290','Computer Organization','CSIT',3),
 ('CMPS 315','System Administration','CSIT',3),
+('CMPS 329','Computer Networking and Security I','CSIT',3),
+('CMPS 339','Database Administration','CSIT',3),
+('CMPS 375','Computer Architecture','CSIT',3),
+('CMPS 383','Inforamtion Systems','CSIT',3),
+('CMPS 390','Data Structures','CSIT',3),
+('CMPS 411','Capstone I','CSIT',3),
+('CMPS 415','Integrated Techologies for Enterprise Systems','CSIT',3),
 ('CMPS 420','Human Computer Interaction','CSIT',3),
-('CMPS 431','Operating System','CSIT',3);
-
+('CMPS 431','Operating Systems','CSIT',3),
+('CMPS 482','Current Trends in Computer Science','CSIT',3),
+('CMPS 409','Computer Networking and Security II','CSIT',3);
 insert into PREREQ
 (C_Nbr,P_Nbr)
 values
-('CMPS 339','CMPS 280'),
-('CMPS 161','MATH 161'),
+('CMPS 161','MATH 155'),
+('CMPS 257','CMPS 161'),
+('CMPS 280','CMPS 161'),
 ('CMPS 285','CMPS 280'),
+('CMPS 290','CMPS 161'),
+('CMPS 294','CMPS 280'),
 ('CMPS 315','CMPS 285'),
+('CMPS 329','CMPS 285'),
+('CMPS 339','CMPS 280'),
+('CMPS 375','CMPS 290'),
+('CMPS 383','CMPS 285'),
+('CMPS 390','CMPS 280'),
+('CMPS 391','CMPS 280'),
+('CMPS 411','CMPS 390'),
+('CMPS 415','CMPS 390'),
 ('CMPS 420','CMPS 390'),
-('CMPS 431','CMPS 375');
+('CMPS 431','CMPS 375'),
+('CMPS 482','CMPS 411'),
+('CMPS 409','CMPS 429');
 
 insert into TAKE
 (W_Nbr,Sec_Id,Grade)
@@ -71,23 +81,41 @@ values
 ('w0569874',3,'B'),
 ('w3698745',5,null);
 
-insert into TIME_SLOT
-(Id,Sec_Nbr,Day,Start_Time,End_Time)
-values
-(14,1,'Monday','9:30am','10:45am'),
-(5,2,'Tuesday','10:00am','10:50am'),
-(10,3,'Wednesday','5:00pm','7:45pm'),
-(13,4,'Thursday','3:30pm','4:45pm'),
-(15,5,'Friday','2:00pm','3:15pm'),
-(21,6,'Monday','3:30pm','4:15pm');
-
 insert into SECTION
-(Id,C_Nbr,Sec_Nbr,Semester,Year,Building,Room_Nbr,TimeSlot_Id,Instructor_Id)
+(C_Nbr,Sec_Nbr,Semester,Year,Building,Room_Nbr,Instructor_Id)
 values
-('CMPS 339',1,'Fall',2016,'Fayard Hall',122,14,4),
-('CMPS 161',3,'Spring',2000,'McGehee Hall',216,5,2),
-('CMPS 285',1,'Fall',2015,'Fayard Hall',107,10,1),
-('CMPS 315',2,'Fall',2016,'Fayard Hall',122,13,5),
-('CMPS 420',1,'Fall',2016,'Fayard Hall',213,15,4),
-('CMPS 431',1,'Spring',2016,'Fayard Hall',218,21,3);
+('CMPS 339',1,'Fall',2016,'Fayard Hall',122,4),
+('CMPS 161',2,'Spring',2000,'McGehee Hall',216,2),
+('CMPS 285',1,'Fall',2015,'Fayard Hall',107,1),
+('CMPS 315',2,'Fall',2016,'Fayard Hall',122,5),
+('CMPS 420',1,'Fall',2016,'Fayard Hall',213,4),
+('CMPS 431',2,'Spring',2016,'Fayard Hall',218,3);
+
+insert into MAJOR
+(Major,Req_Course)
+values
+('Information Technology','CMPS 161'),
+('Information Technology','CMPS 257'),
+('Information Technology','CMPS 280'),
+('Information Technology','CMPS 285'),
+('Information Technology','CMPS 290'),
+('Information Technology','CMPS 294'),
+('Information Technology','CMPS 315'),
+('Information Technology','CMPS 329'),
+('Information Technology','CMPS 339'),
+('Information Technology','CMPS 375'),
+('Information Technology','CMPS 383'),
+('Information Technology','CMPS 390'),
+('Information Technology','CMPS 391'),
+('Information Technology','CMPS 411'),
+('Information Technology','CMPS 415'),
+('Information Technology','CMPS 420'),
+('Information Technology','CMPS 431'),
+('Information Technology','CMPS 482'),
+('Information Technology','CMPS 409');
+
+
+
+
+
 
